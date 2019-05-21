51a730aa6aa53751b6ae3c321164d061f39ec29e
kindring <43570639+kindring@users.noreply.github.com>
2019-05-21 21:32:52 +0800

    启动文件

---
+++ app.js
@@ -0,0 +1,10 @@
+const ht=require('./storeServer.js');
+const path=require('path');
+const router=require('./router/router.js');
+ht.use('/imgs', path.join(__dirname,'./views/imgs/'));
+ht.use('/css', path.join(__dirname,'./views/css/'));
+ht.use('/js', path.join(__dirname,'./views/js/'));
+ht.listen(80,'127.0.0.1',function(){
+   console.log('server is runing');
+});
+
